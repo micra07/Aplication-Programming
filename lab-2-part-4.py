3f963feacce37a245e18dabf7abcff8caa4b278d
micra07 <126266110+micra07@users.noreply.github.com>
2023-12-11 19:46:05 +0400

    lab-2-part-4.py

---
+++ lab-2-part-4.py
@@ -0,0 +1,15 @@
+import os
+
+def Get_Next(name: str) -> str:
+    path = os.path.join('dataset', name)
+    img_names = os.listdir(path)
+    img_names.append(None)
+    
+    for i in range(len(img_names)):
+        if img_names[i] is not None:
+            yield os.path.join(path, img_names[i])
+        elif img_names[i] is None:
+            yield None   
+
+if __name__ == "__main__":
+    print(*Get_Next('brownbear'))
